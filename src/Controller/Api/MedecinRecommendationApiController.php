839d776e6b1dc11fafd6fa272d6447245aee1c9c
Ilefjemai568 <ilefjemai8@gmail.com>
2026-02-21 23:28:25 +0100

    modification

--- src/Controller/Api/MedecinRecommendationApiController.php
+++ src/Controller/Api/MedecinRecommendationApiController.php
@@ -81,6 +81,18 @@ class MedecinRecommendationApiController extends AbstractController
             $maxDistanceKm
         );
 
+        $usedFallback = false;
+        if (empty($recommendations)) {
+            $recommendations = $recommendationService->recommendNearest(
+                $medecins,
+                $patientLat,
+                $patientLng,
+                $limit,
+                null
+            );
+            $usedFallback = !empty($recommendations);
+        }
+
         return new JsonResponse([
             'success' => true,
             'patient_location' => [
@@ -89,6 +101,7 @@ class MedecinRecommendationApiController extends AbstractController
                 'captured_at' => (string) ($location['captured_at'] ?? ''),
             ],
             'count' => count($recommendations),
+            'fallback_no_radius' => $usedFallback,
             'recommendations' => $recommendations,
         ]);
     }
